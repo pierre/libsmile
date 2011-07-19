943ce7f80e4dfff0d0f2c84c8d04b0de12917f28
Pierre-Alexandre Meyer <pierre@ning.com>
2011-07-19 07:07:35 -0700

    main: add basic String decoding
    Managed to parse a raw Smile file, and extract the keys.
    With back references, this code extracts the schema from the file.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- smile.c
+++ smile.c
@@ -1,137 +1,124 @@
-/* http://wiki.fasterxml.com/JacksonBinaryFormatSpec */
 /*
-Constant byte #0: 0x3A (ASCII ':')
-Constant byte #1: 0x29 (ASCII ')')
-Constant byte #2: 0x0A (ASCII linefeed, '\n')
-Variable byte #3, consisting of bits:
-Bits 4-7 (4 MSB): 4-bit version number; 0x00 for current version (note: it is possible that some bits may be reused if necessary)
-Bits 3: Reserved
-Bit 2 (mask 0x04) Whether raw binary (unescaped 8-bit) values may be present in content
-Bit 1 (mask 0x02): Whether shared String value checking was enabled during encoding -- if header missing, default value of "false" must be assumed for decoding (meaning parser need not store decoded String values for back referencing)
-Bit 0 (mask 0x01): Whether shared property name checking was enabled during encoding -- if header missing, default value of "true" must be assumed for decoding (meaning parser MUST store seen property names for possible back references)
-*/
+ * Copyright 2011 Pierre-Alexandre Meyer
+ *
+ * Pierre-Alexandre Meyer licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+/* http://wiki.fasterxml.com/JacksonBinaryFormatSpec */
+
+#include <errno.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 #include "smile.h"
 
-#define SUCCESS 0
+// Usage: gcc -Wall smile.c -o smile && ./smile data.file
+// Decodes raw smile
+int main(int argc, char *argv[])
+{
+    int fd, rc = 0;
+    struct stat mystat;
+    const char* fname = argv[1];
+    size_t nbytes;
+    ssize_t bytes_read;
+    u8 buf[BUFFER_SIZE];
+    char header[4];
+    const u8* ip;
+    u8* eob;
+    int length = 0;
 
-// Smile Tokens
-#define SMILE_EOS 0xFF
-#define SMILE_EMPTY_STRING 0x20
-#define SMILE_NULL 0x21
-#define SMILE_TRUE 0x22
-#define SMILE_FALSE 0x23
+    nbytes = sizeof(buf);
 
-#define SMILE_START_OBJECT 0xFA
-#define SMILE_END_OBJECT 0xFB
+    rc = stat(fname, &mystat);
+    fd = open(fname, O_RDONLY);
 
-// buffer bits
-
-struct smile_stream_t {
-    size_t location;
-    size_t size;
-    unsigned char *buffer;
-};
-
-smile_stream* smile_stream_create(const char* string) {
-    smile_stream *s = malloc(sizeof(smile_stream));
-    s->location = 0;
-    s->size = strlen(string);
-    s->buffer = (unsigned char*)strdup(string);
-    return s;
-}
-
-int smile_stream_destroy(smile_stream* stream) {
-    return SUCCESS;
-}
-
-
-
-// returns SMILE_EOS at end of stream
-static int ss_getc(unsigned char* c, smile_stream *s) {
-    *c = s->buffer[s->location++];
-    return SUCCESS;
-}
-
-// Parser bits
-
-typedef struct {
-    unsigned char flags;
-} parser_state;
-
-
-static int consume_next_value(parser_state *ps, smile_stream *s);
-
-
-static int consume_object(parser_state *ps, smile_stream *s) {
-    
-    return consume_next_value(ps, s);
-}
-
-static int consume_next_value(parser_state *ps, smile_stream *s) {
-    unsigned char c;
-    int rv;
-    if ((rv = ss_getc(&c, s))) return rv;
-    printf("TOKEN TYPE %x\n", c);
-    switch (c) {
-        case SMILE_EOS:
-            printf("end of stream\n");
-            return SUCCESS;
-        case SMILE_START_OBJECT:
-            printf("start object\n");
-            return consume_object(ps, s);
-        case SMILE_NULL:
-            printf("null\n");
-            return consume_next_value(ps, s);
-        case SMILE_EMPTY_STRING:
-            printf("empty string\n");
-            return consume_next_value(ps, s);
-        case SMILE_TRUE:
-            printf("true\n");
-            return consume_next_value(ps, s);
-        case SMILE_FALSE:
-            printf("true\n");
-            return consume_next_value(ps, s);
-        default:
-            printf("DEFAULT OOPS\n");
-            return 1;
+    if (fd == -1) {
+        fprintf (stderr, "%s: not a regular file.\n", fname);
+        exit(1);
     }
-}
-
-static int consume_header(parser_state *ps, smile_stream *s) {
-    unsigned char c;
-    int rv;
-    if ((rv = ss_getc(&c, s)) || c != ':') return rv;
-    if ((rv = ss_getc(&c, s)) || c != ')') return rv;
-    if ((rv = ss_getc(&c, s)) || c != '\n') return rv;
-    if ((rv = ss_getc(&c, s))) return rv;
-    ps->flags = c;
-    
-    return consume_next_value(ps, s);
-}
-
-int smile_parse(smile_stream *stream) {
-    int rv;
-    parser_state ps;
-    if ((rv = consume_header(&ps, stream))) return rv;
-    
-    return SUCCESS;
-}
 
 
-// test bits
-
-int main()
-{    
-    // HEADER START_OBJECT EOS
-    smile_stream *c2 = smile_stream_create(":)\nX\xFA\xFF");
-    
-    if (smile_parse(c2)) {
-        printf("failure\n");
+    bytes_read = read(fd, header, 4);
+    if (bytes_read != 4) {
+          fprintf(stderr, "%s: unable to read header: \n", fname);
+          perror ("");
+          goto exit;
+    } else {
+        if (!(header[0] == ':' && header[1] == ')' && header[2] == '\n')) {
+            fprintf(stderr, "%s: bad header: %s\n", fname, header);
+            goto exit;
+        } else {
+            printf("Got header: %s", header);
+        }
     }
-    else {
-        printf("success\n");
+
+    while (1) {
+        // Read block by block
+        bytes_read = read(fd, buf, nbytes);
+        if (bytes_read == -1) {
+          fprintf(stderr, "%s: read error: \n", fname);
+          perror ("");
+          goto exit;
+        } else if (bytes_read == 0) {
+            break;
+        }
+
+        fprintf(stderr, "Read %lu bytes\n", (unsigned long int) bytes_read);
+        ip = buf;
+        eob = buf + nbytes;
+        while (ip < eob) {
+            if (*ip == (char)0x00) {
+                // nothing
+            }
+            else if (*ip == SMILE_START_OBJECT) {
+                putchar('{');
+                putchar('\n');
+            } else if (*ip == SMILE_END_OBJECT) {
+                putchar('}');
+            } else if  (*ip == SMILE_START_ARRAY) {
+                putchar('[');
+                putchar('\n');
+            } else if (*ip == SMILE_END_ARRAY) {
+                putchar(']');
+            } else {
+                // Tiny Unicode, Small Unicode
+                if (*ip >= (u8) 0x80 && *ip <= (u8) 0xBF) {
+                    length = (*ip & 0x1F);
+                    ip++;
+                    putchar('"');
+                    // humm, why +1?
+                    while (length + 1) {
+                        putchar(*ip);
+                        ip++;
+                        length--;
+                    }
+                    putchar('"');
+                } else {
+                    // TODO
+                }
+            }
+            ip++;
+        }
     }
+
+exit:
+    close(fd);
+    puts("Done!");
+    exit(0);
 }
--- smile.h
+++ smile.h
@@ -1,11 +1,24 @@
 #ifndef _SMILE_H_
 #define _SMILE_H_
 
-typedef struct smile_stream_t smile_stream;
+typedef unsigned char u8;
 
-int smile_parse(smile_stream *stream);
+#define DEBUG_BYTE(byte) printf("0x%02x ", (u8) byte);
 
-smile_stream* smile_stream_create(const char* string);
-int smile_stream_destroy(smile_stream* stream);
+#define SUCCESS 0
+
+#define BUFFER_SIZE 1024
+
+// Smile Tokens
+#define SMILE_EOS (u8) 0xFF
+#define SMILE_EMPTY_STRING (u8) 0x20
+#define SMILE_NULL (u8) 0x21
+#define SMILE_TRUE (u8) 0x22
+#define SMILE_FALSE (u8) 0x23
+
+#define SMILE_START_ARRAY (u8) 0xF8
+#define SMILE_END_ARRAY (u8) 0xF9
+#define SMILE_START_OBJECT (u8) 0xFA
+#define SMILE_END_OBJECT (u8) 0xFB
 
 #endif /* !_SMILE_H_ */

e2bd6f0b9b21929bfb74b2198f5e9eebf470d4fb
Pierre-Alexandre Meyer <pierre@mouraf.org>
2011-11-23 20:38:09 -0800

    c: fix build
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- c/src/printer.h
+++ c/src/printer.h
@@ -3,10 +3,15 @@
 
 void print_start_object();
 void print_end_object();
+void print_start_array();
+void print_end_array();
 void print_start_key();
 void print_end_key();
 void print_start_value();
 void print_end_value();
-void print_value(const u8*, int, int);
+void print_null_value();
+void print_false_value();
+void print_true_value();
+void print_number_value(long);
 
 #endif
